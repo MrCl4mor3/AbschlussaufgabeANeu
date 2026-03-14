f39942c1a9759b1b30f697df008e0d75d5035713
jonas <jonasmanzl99@web.de>
2026-03-14 20:05:50 +0100

    JavaDoc

--- src/edu/kit/kastel/crownoffarmland/gameplay/snapshots/SnapshotProvider.java
+++ src/edu/kit/kastel/crownoffarmland/gameplay/snapshots/SnapshotProvider.java
@@ -7,11 +7,46 @@ import edu.kit.kastel.crownoffarmland.model.team.TeamID;
 
 import java.util.List;
 
+/**
+ * Interface.
+ *
+ *
+ * @author ucgdi
+ */
 public interface SnapshotProvider {
 
+    /**
+     * w.
+     * @return The snapshot of the whole board.
+     */
     BoardSnapshot createBoardSnapshot();
+
+    /**
+     * w.
+     * @return an Entity Snapshot.
+     * @throws InvalidGameStateException if the gamestate is invalid for creating the snapshot, e.g. if no field is selected for
+     *      createEntitySnapshotAtSelected.
+     */
     EntitySnapshot createEntitySnapshot() throws InvalidGameStateException;
+
+    /**
+     * W.
+     * @return w
+     * @throws InvalidGameStateException w
+     */
     EntityOnPositionSnapshot createEntitySnapshotAtSelected() throws InvalidGameStateException;
+
+    /**
+     * w.
+     * @return w
+     */
     List<EntitySnapshot> createHandSnapshot();
+
+    /**
+     * w.
+     * @param teamID w
+     * @return w
+     * @throws InvalidGameStateException w
+     */
     TeamStateSnapshot createTeamStateSnapshot(TeamID teamID) throws InvalidGameStateException;
 }
