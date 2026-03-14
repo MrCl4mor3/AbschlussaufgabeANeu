9fe19f0464ee98a341e1b1034397b35e55750bb9
jonas <jonasmanzl99@web.de>
2026-03-14 20:08:53 +0100

    JavaDoc hinzugefügt.

--- src/edu/kit/kastel/crownoffarmland/model/team/GameTeamView.java
+++ src/edu/kit/kastel/crownoffarmland/model/team/GameTeamView.java
@@ -1,15 +1,46 @@
 package edu.kit.kastel.crownoffarmland.model.team;
 
 
-
+/**
+ * Interface.
+ *
+ * @author ucgdi
+ */
 public interface GameTeamView {
+    /**
+     * Getter for the name of a team.
+     * @return the Name of the selected team.
+     */
     String getName();
+
+    /**
+     * Getter for Life Points.
+     * @return The current Life Points of the selected team.
+     */
     int getLifePoints();
 
+    /**
+     * Getter for the HandSize.
+     * @return the current Size of the hand.
+     */
     int getHandSize();
+
+    /**
+     * Check if the hand is full.
+     * @return true, if the hand is full, false otherwise
+     */
     boolean isHandFull();
 
+    /**
+     * Getter for the Size of the DrawPile.
+     * @return The Size of the Draw Pile
+     */
     int getDrawPileSize();
+
+    /**
+     * Check if the Draw Pile is empty.
+     * @return true, if the DrawPile is empty, false otherwise
+     */
     boolean isDrawPileEmpty();
 }
 
